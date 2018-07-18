453b5cb1aaf75cc82c13f4a7bb0b7b45d2a216ae
Tom Dugan <thomas@aadwebb.com>
2018-07-17 19:13:21 -0500

    all sections displaying

---
+++ src/components/MyCollection/MyCollection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyCollection.css';
+
+class MyCollection extends React.Component {
+  render () {
+    return (
+      <div className="MyCollection">
+        <h1>MyCollection</h1>
+      </div>
+    );
+  }
+};
+
+export default MyCollection;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Search.css';
+
+class Search extends React.Component {
+  render () {
+    return (
+      <div className="Search">
+        <h1>Search</h1>
+      </div>
+    );
+  }
+};
+
+export default Search;
