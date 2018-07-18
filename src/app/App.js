a128fdc31529bc38947a1db78db37139aa345ab0
Tom Dugan <thomas@aadwebb.com>
2018-07-18 18:16:37 -0500

    starting point

--- src/app/App.js
+++ src/app/App.js
@@ -1,106 +1,26 @@
 import React, { Component } from 'react';
-import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
-import firebase from 'firebase';
 
 import './App.css';
 
-// import Home from '../components/Home/Home';
+import Home from '../components/Home/Home';
 import Login from '../components/Login/Login';
 import MyCollection from '../components/MyCollection/MyCollection';
 import Navbar from '../components/Navbar/Navbar';
-// import NewGame from '../components/NewGame/NewGame';
+import NewGame from '../components/NewGame/NewGame';
 import Registration from '../components/Registration/Registration';
-// import Search from '../components/Search/Search';
-import fbConnection from '../firebaseRequests/connection';
-fbConnection();
-
-const PrivateRoute = ({ component: Component, authed, ...rest}) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authed === true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: '/login', state: {from: props.location}}}
-          />
-        )
-      }
-    />
-  );
-};
-
-const PublicRoute = ({ component: Component, authed, ...rest}) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authed === false ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: '/mycollection', state: {from: props.location}}}
-          />
-        )
-      }
-    />
-  );
-};
+import Search from '../components/Search/Search';
 
 class App extends Component {
-  state={
-    authed: false,
-  }
-
-  componentDidMount () {
-    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({authed: true});
-      } else {
-        this.setState({authed: false});
-      }
-    });
-  }
-
-  componentWillUnmount () {
-    this.removeListener();
-  }
-
-  runAway = () => {
-    this.setState({authed: false});
-  }
-
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <div>
-            <Navbar />
-            <div className="container">
-              <div className="row">
-                <Switch>
-                  <Route path="/" exact component={Registration} />
-                  <PrivateRoute
-                    path="/mycollection"
-                    authed={this.state.authed}
-                    component={MyCollection}
-                  />
-                  <PublicRoute
-                    path="/registration"
-                    authed={this.state.authed}
-                    component={Registration}
-                  />
-                  <PublicRoute
-                    path="/login"
-                    authed={this.state.authed}
-                    component={Login}
-                  />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </BrowserRouter>
+        <Home />
+        <Login />
+        <MyCollection />
+        <Navbar />
+        <NewGame />
+        <Registration />
+        <Search />
       </div>
     );
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,98 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import authRequests from '../../firebaseRequests/auth';
 
 import './Login.css';
 
 class Login extends React.Component {
-  state={
-    user: {
-      email: 'tom@tom.com',
-      password: 'password',
-    },
-  };
-
-  loginClickEvent = (e) => {
-    const { user } = this.state;
-    e.preventDefault();
-    authRequests
-      .loginUser(user)
-      .then(() => {
-        this.props.history.push('/mycollection');
-      })
-      .catch(errror => {
-        console.error('Error logging user in.', errror);
-      });
-  };
-
-  emailChange = e => {
-    const tempUser = { ...this.state.user };
-    tempUser.email = e.target.value;
-    this.setState({ user: tempUser });
-  };
-
-  passwordChange = e => {
-    const tempUser = { ...this.state.user };
-    tempUser.password = e.target.value;
-    this.setState({ user: tempUser });
-  };
-
   render () {
-    const { user } = this.state;
     return (
       <div className="Login">
-        <div id="login-form">
-          <h1 className="text-center">Login</h1>
-          <form className="form-horizontal">
-            <div className="form-group">
-              <label htmlFor="inputEmail" className="control-label">
-                Email:
-              </label>
-              <div>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="inputEmail"
-                  placeholder=""
-                  value={user.email}
-                  onChange={this.emailChange}
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="inputPassword" className="control-label">
-                Password:
-              </label>
-              <div>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="inputPassword"
-                  placeholder=""
-                  value={user.password}
-                  onChange={this.passwordChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div>
-                <Link to="/registration">Need to Register?</Link>
-              </div>
-            </div>
-            <div className="form-group">
-              <div>
-                <button
-                  type="submit"
-                  className="btn btn-default"
-                  onClick={this.loginClickEvent}
-                >
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+        <h1>Login</h1>
       </div>
     );
   }
