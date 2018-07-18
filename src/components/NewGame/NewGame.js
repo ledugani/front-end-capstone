a32e56ce2178a78ef2a44d5e914c5eaf46a8d107
Tom Dugan <thomas@aadwebb.com>
2018-07-17 19:13:21 -0500

    all sections displaying

---
+++ src/components/NewGame/NewGame.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NewGame.css';
+
+class NewGame extends React.Component {
+  render () {
+    return (
+      <div className="NewGame">
+        <h1>NewGame</h1>
+      </div>
+    );
+  }
+};
+
+export default NewGame;
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Registration.css';
+
+class Registration extends React.Component {
+  render () {
+    return (
+      <div className="Registration">
+        <h1>Registration</h1>
+      </div>
+    );
+  }
+};
+
+export default Registration;
