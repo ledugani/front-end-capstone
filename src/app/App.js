28b00bf8f988b11554f6e0fb415fef0fdc85ec8d
Tom Dugan <thomas@aadwebb.com>
2018-07-18 18:47:08 -0500

    enabled private routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 
 import './App.css';
 
 import Home from '../components/Home/Home';
-import Login from '../components/Login/Login';
-import MyCollection from '../components/MyCollection/MyCollection';
 import Navbar from '../components/Navbar/Navbar';
-import NewGame from '../components/NewGame/NewGame';
-import Registration from '../components/Registration/Registration';
+// import Login from '../components/Login/Login';
+// import MyCollection from '../components/MyCollection/MyCollection';
+// import NewGame from '../components/NewGame/NewGame';
+// import Registration from '../components/Registration/Registration';
 import Search from '../components/Search/Search';
 
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state={
+    authed: false,
+  }
   render() {
     return (
       <div className="App">
-        <Home />
-        <Login />
-        <MyCollection />
-        <Navbar />
-        <NewGame />
-        <Registration />
-        <Search />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home} />
+                  <PrivateRoute
+                    path="/search"
+                    authed={this.state.authed}
+                    component={Search}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
