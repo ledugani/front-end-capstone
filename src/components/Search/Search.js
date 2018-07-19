d80e39257eed5455c5dc5c89945f8b06b1b3b2c4
Tom Dugan <thomas@aadwebb.com>
2018-07-18 20:06:13 -0500

    prints game titles to page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import gameRequests from '../../firebaseRequests/games';
 
 import './Search.css';
 
 class Search extends React.Component {
+  state = {
+    games: [],
+  }
+
+  componentDidMount () {
+    gameRequests
+      .getRequest()
+      .then((games) => {
+        this.setState({games});
+      })
+      .catch((err) => {
+        console.error('There was a problem requesting game info.', err);
+      })
+  }
+
   render () {
+    const gameComponents = this.state.games.map((game) => {
+      return (
+        <h2>{game.title}</h2>
+      )
+    })
     return (
-      <div className="Search">
-        <h1>Search</h1>
+      <div className="search-component">
+        <div className="Search">
+          <h1>Search</h1>
+        </div>
+        <div className="More">
+          <h1>More Games</h1>
+          <ul className="games">
+            {gameComponents}
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/firebaseRequests/games.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import constants from '../constants';
+
+const getRequest = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${constants.firebaseConfig.databaseURL}/games.json`)
+      .then(results => {
+        const games = [];
+        if (results.data !== null) {
+          Object.keys(results.data).forEach(fbKey => {
+            results.data[fbKey].id = fbKey;
+            games.push(results.data[fbKey]);
+          });
+        }
+        resolve(games);
+      })
+      .catch(errrorr => {
+        reject(errrorr);
+      });
+  });
+};
+
+export default { getRequest };
